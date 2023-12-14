3b629d90ad1f3633c1c0af0f06b7f673f7a32a6a
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2023-12-14 16:06:40 +0800

    seata：TCC模式

--- account-service/src/main/java/cn/itcast/account/service/AccountTCCService.java
+++ account-service/src/main/java/cn/itcast/account/service/AccountTCCService.java
@@ -1,13 +1,19 @@
 package cn.itcast.account.service;
 
-//@LocalTCC
-//public interface AccountTCCService {
-//
-//    @TwoPhaseBusinessAction(name = "deduct", commitMethod = "confirm", rollbackMethod = "cancel")
-//    void deduct(@BusinessActionContextParameter(paramName = "userId") String userId,
-//                @BusinessActionContextParameter(paramName = "money")int money);
-//
-////    boolean confirm(BusinessActionContext ctx);
-////
-////    boolean cancel(BusinessActionContext ctx);
-//}
+import io.seata.rm.tcc.api.BusinessActionContext;
+import io.seata.rm.tcc.api.BusinessActionContextParameter;
+import io.seata.rm.tcc.api.LocalTCC;
+import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
+@LocalTCC
+public interface AccountTCCService {
+
+    /*TwoPhaseBusinessAction写在哪个方法，哪个就是try，BusinessActionContextParameter的属性可以在上线文BusinessActionContext中传递*/
+    @TwoPhaseBusinessAction(name = "deduct", commitMethod = "confirm", rollbackMethod = "cancel")
+    void deduct(@BusinessActionContextParameter(paramName = "userId") String userId,
+                @BusinessActionContextParameter(paramName = "money")int money);
+
+    boolean confirm(BusinessActionContext ctx);
+
+    boolean cancel(BusinessActionContext ctx);
+}
--- account-service/src/main/java/cn/itcast/account/service/impl/AccountTCCServiceImpl.java
+++ account-service/src/main/java/cn/itcast/account/service/impl/AccountTCCServiceImpl.java
@@ -1,57 +1,80 @@
-//package cn.itcast.account.service.impl;
-//
-//import cn.itcast.account.mapper.AccountMapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//@Slf4j
-//public class AccountTCCServiceImpl {
-//
-//    @Autowired
-//    private AccountMapper accountMapper;
-////    @Autowired
-////    private AccountFreezeMapper freezeMapper;
-//
-//
-//    @Transactional
-//    public void deduct(String userId, int money) {
-//        // 0.获取事务id
-////        String xid = RootContext.getXID();
-//        // 1.扣减可用余额
-//        accountMapper.deduct(userId, money);
-//        // 2.记录冻结金额，事务状态
-////        AccountFreeze freeze = new AccountFreeze();
-////        freeze.setUserId(userId);
-////        freeze.setFreezeMoney(money);
-////        freeze.setState(AccountFreeze.State.TRY);
-////        freeze.setXid(xid);
-////        freezeMapper.insert(freeze);
-//    }
-//
-////    @Override
-////    public boolean confirm(BusinessActionContext ctx) {
-////        // 1.获取事务id
-////        String xid = ctx.getXid();
-////        // 2.根据id删除冻结记录
-////        int count = freezeMapper.deleteById(xid);
-////        return count == 1;
-////    }
-//
-////    @Override
-////    public boolean cancel(BusinessActionContext ctx) {
-////        // 0.查询冻结记录
-////        String xid = ctx.getXid();
-////        AccountFreeze freeze = freezeMapper.selectById(xid);
-////
-////        // 1.恢复可用余额
-////        accountMapper.refund(freeze.getUserId(), freeze.getFreezeMoney());
-////        // 2.将冻结金额清零，状态改为CANCEL
-////        freeze.setFreezeMoney(0);
-////        freeze.setState(AccountFreeze.State.CANCEL);
-////        int count = freezeMapper.updateById(freeze);
-////        return count == 1;
-////    }
-//}
+package cn.itcast.account.service.impl;
+
+import cn.itcast.account.entity.AccountFreeze;
+import cn.itcast.account.mapper.AccountFreezeMapper;
+import cn.itcast.account.mapper.AccountMapper;
+import cn.itcast.account.service.AccountTCCService;
+import io.seata.core.context.RootContext;
+import io.seata.rm.tcc.api.BusinessActionContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class AccountTCCServiceImpl implements AccountTCCService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+    @Autowired
+    private AccountFreezeMapper freezeMapper;
+
+    @Override
+    @Transactional
+    public void deduct(String userId, int money) {
+        // 0.获取事务id
+        String xid = RootContext.getXID();
+        //避免业务悬挂避免已经提交了事务了，才开始try
+        if (freezeMapper.selectById(xid) != null){
+            return;
+        }
+        // 1.扣减可用余额
+        accountMapper.deduct(userId, money);
+        // 2.记录冻结金额，事务状态
+        AccountFreeze freeze = new AccountFreeze();
+        freeze.setUserId(userId);
+        freeze.setFreezeMoney(money);
+        freeze.setState(AccountFreeze.State.TRY);
+        freeze.setXid(xid);
+        freezeMapper.insert(freeze);
+    }
+
+    @Override
+    public boolean confirm(BusinessActionContext ctx) {
+        // 1.获取事务id
+        String xid = ctx.getXid();
+        // 2.根据id删除冻结记录
+        int count = freezeMapper.deleteById(xid);
+        return count == 1;
+    }
+
+    @Override
+    public boolean cancel(BusinessActionContext ctx) {
+        // 0.查询冻结记录
+        String xid = ctx.getXid();
+        AccountFreeze freeze = freezeMapper.selectById(xid);
+        //空回滚判断，freeze为null，证明try并没有执行，需要空回滚
+        if (freeze == null){
+            AccountFreeze freezeNew = new AccountFreeze();
+            freezeNew.setUserId(ctx.getActionContext("userId").toString());
+            freezeNew.setFreezeMoney(0);
+            freezeNew.setState(AccountFreeze.State.TRY);
+            freezeNew.setXid(xid);
+            freezeMapper.insert(freezeNew);
+            return true;
+        }
+        //2.幂等判断
+        if (AccountFreeze.State.CANCEL == freeze.getState()){
+            log.info("已经执行过cancel了");
+            return true;
+        }
+        // 1.恢复可用余额
+        accountMapper.refund(freeze.getUserId(), freeze.getFreezeMoney());
+        // 2.将冻结金额清零，状态改为CANCEL
+        freeze.setFreezeMoney(0);
+        freeze.setState(AccountFreeze.State.CANCEL);
+        int count = freezeMapper.updateById(freeze);
+        return count == 1;
+    }
+}
